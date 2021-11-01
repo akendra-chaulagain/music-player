2448aaee9050d987e948f9d4b06f882ee4c15358
Akendra-Chaulagain <yakendrachaulagain31@gmail.com>
2021-11-01 08:29:45 +0545

    music player

---
+++ music-list.js
@@ -0,0 +1,42 @@
+
+let allMusic = [
+    {
+      name: "Harley Bird - Home",
+      artist: "Arjit-singh",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Khudaya Khair",
+      artist: " Soham Chakrabarthy, Akruti ",
+      img: "music-2",
+      src: "music-2"
+    },
+
+
+    {
+      name: "Pee Loon Hoto Ki Sargam",
+      artist: "Mohit Chauhan",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Perfrct",
+      artist: "One Direction",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Bad Habits",
+      artist: "Ed-sheeran",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "The Night",
+      artist: " Avicii ",
+      img: "music-6",
+      src: "music-6"
+    },
+    
+  ];
